8f9334c205b48617b058d4150a9c61045be80163
Lucas Coutinho <CucasLoutinho@gmail.com>
2020-01-09 14:53:01 -0300

    considcional simples

---
+++ Condicional-simples.py
@@ -0,0 +1,10 @@
+# Programa para verificar se um número é par ou ímpar
+
+# Declaração da variável
+numero = 7
+
+# Estrutura condicional
+if numero % 2 == 0:
+    print("O número", numero, "é PAR")
+else:
+    print("O número", numero, "é ÍMPAR")
